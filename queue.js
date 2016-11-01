6897db99d4af77c47532cea219a19c68b870bc57
xiaowei-vip <admin@daniu.org>
2016-11-01 11:37:20 +0800

    Update queue.js

--- queue.js
+++ queue.js
@@ -9,7 +9,7 @@ var request = require('request');
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync('./config.json').toString());
 var redis_client = redis.createClient(config.redis_port,config.redis_host); //creates a new client
-console.log(Date().Format(“yyyy-MM-dd HH:mm:ss”));
+console.log(Date());
 console.log("后台队列服务已经启动，随时等待新队列任务");
 
 
@@ -31,6 +31,7 @@ function check_queue_hash(){
 				var single_attribute = JSON.parse(queue_list[i+1]);
 				//判断队列是否在操作中。如果是则不重复启动，以保证同一个队列内的任务是按顺序one by one 执行的
 				if (queue_status_array &&  queue_status_array[single_queue_name] > 0 ) {
+					console.log(Date());
 					console.log('队列'+single_queue_name+'正操作中，不重复启动');
 				}else{
 					//执行单个队列single_queue_name
@@ -61,7 +62,7 @@ function run_queue(queue_name,attribute){
 				redis_client.hget(times_queue,url,function(err, reply){
 					var execution_times = parseInt(reply) ? parseInt(reply) : 0 ;
 					request(request_url, function (error, response, body) {
-						console.log(Date().Format(“yyyy-MM-dd HH:mm:ss”));
+						console.log(Date());
 						console.log("第"+(execution_times+1)+"次执行来自"+queue_name+"的url："+request_url);
 						if (body == "done") {
 							//删除任务
@@ -78,7 +79,8 @@ function run_queue(queue_name,attribute){
 							var offset = execution_times*attribute.stepping_time*1000 ;
 							offset = offset > attribute.max_time_interval*1000 ? attribute.max_time_interval*1000 :offset ;
 							next_time = Date.parse(new Date()) + offset ;
-							////console.log("推迟到"+next_time+"执行");
+							console.log(Date());
+							console.log("推迟到"+next_time+"执行");
 							redis_client.zadd(queue_name,next_time,url);
 							redis_client.hset(times_queue,url,execution_times+1);
 
@@ -96,7 +98,7 @@ function run_queue(queue_name,attribute){
 				//删除这个元素。不在执行url后在删除是为了防止因为执行不了url而造成阻塞
 				redis_client.zrem(queue_name,url,function(){
 					request(request_url, function (error, response, body) {
-						console.log(Date().Format(“yyyy-MM-dd HH:mm:ss”));
+						console.log(Date());
 						console.log("执行来自"+queue_name+"的url："+request_url);
 						//queue_status_array[queue_name] = 0 ;
 						run_queue(queue_name,attribute);
